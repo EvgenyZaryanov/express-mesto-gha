8931415bbd23e8cad821d2a1076a7fdd3e78c91c
Evgeny Zaryanov <eugenezaryanov@yandex.ru>
2023-10-24 16:31:56 +0300

    for the 1st review-sprint-14

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,7 @@
 const {
   PORT = 3000,
   DB = "mongodb://127.0.0.1:27017/mestodb",
-  JWT_SECRET = "CCD34142A3247B5AFD845A6D6C59C3AD700703C9B6EE930436128D3CF2260DDB",
+  JWT_SECRET = "secret_key",
 } = process.env;
 
 module.exports = {
