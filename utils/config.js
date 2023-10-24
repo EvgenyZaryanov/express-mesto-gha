67f41d037a9f8a4e953e761ac92ffa24044e075b
Evgeny Zaryanov <eugenezaryanov@yandex.ru>
2023-10-24 18:13:07 +0300

    bug fixed

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,7 @@
 const {
   PORT = 3000,
   DB = "mongodb://127.0.0.1:27017/mestodb",
-  JWT_SECRET = "a1d731321cb2024fe9dfa90167bc4b40c2eefd1a4975d641f3d1484f71f5868c",
+  JWT_SECRET = "secret_key",
 } = process.env;
 
 module.exports = {
